6dcff145a2c84512af27e7261111cd9fc0ba9f38
arthurfortunato <luizarthur.fortunato@gmail.com>
2021-10-29 00:26:09 -0300

    Criando a tela de criação de sala

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,8 +1,10 @@
-import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
 
+/* import { Home } from "./pages/Home";
+ */
 export function App() {
   return (
-    <Home />
+    <NewRoom />
   );
 }
 
